5c0e0b25206d11dfcda642e9165be4b40112eae5
AtakltiHanis <atakltihanis14@gmail.com>
2025-02-02 11:22:50 +0300

    side bar Icons

--- src/components/leftbar/Sidebar.jsx
+++ src/components/leftbar/Sidebar.jsx
@@ -1,5 +1,15 @@
 import "./sidebar.css";
-import { RssFeed } from "@mui/icons-material";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@mui/icons-material";
 
 const Sidebar = () => {
   return (
@@ -9,6 +19,242 @@ const Sidebar = () => {
           <li className="sidebarListItem">
             <RssFeed className="sidebarIcon" />
             <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">BookMarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/asset/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Ataklti Hanis</span>
           </li>
         </ul>
       </div>
